deab6cf5d0702dad147d016426de0aa49f8835dc
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-10-06 20:54:14 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,14 +1,19 @@
-#(©)Codexbotz
+#(©)NextGenBotz
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, OWNER_ID
 from helper_func import encode, get_message_id
+from database.database import is_admin
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@Bot.on_message(filters.command('batch') & filters.private)
 async def batch(client: Client, message: Message):
-    while True:
+    user_id = message.from_user.id
+    is_user_admin = await is_admin(user_id)
+    if not is_user_admin and user_id != OWNER_ID:        
+        return
+    while True:       
         try:
             first_message = await client.ask(text = "Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
@@ -39,9 +44,12 @@ async def batch(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
-
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@Bot.on_message(filters.command('genlink') & filters.private)
 async def link_generator(client: Client, message: Message):
+    user_id = message.from_user.id
+    is_user_admin = await is_admin(user_id)
+    if not is_user_admin and user_id != OWNER_ID:        
+        return
     while True:
         try:
             channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
