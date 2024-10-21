75a529dff62a7fbead6f29a8d94517f5cedd6060
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-10-21 06:33:19 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -41,8 +41,8 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    [reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]),
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Premium Channel", url=f'https://t.me/+PY35ZUH_GEQ4MmIx={link}')]])]
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]),
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Premium Channel", url=f'https://t.me/+PY35ZUH_GEQ4MmIx={link}')]])
     await second_message.reply_text(f"<b>📥🔗 — Download Link — 📥🔗\n\nUploaded By:  @Alya_x_Yuki</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 @Bot.on_message(filters.command('genlink') & filters.private)
